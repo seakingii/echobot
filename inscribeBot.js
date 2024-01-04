ff5bfd42b969448e9492f18927985c816d7b2d22
Mr. Ozuru <132787858+mrozuru@users.noreply.github.com>
2024-01-04 11:58:56 -0500

    Update inscribeBot.js

--- inscribeBot.js
+++ inscribeBot.js
@@ -13,7 +13,7 @@ const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY, provider); // Be very 
 const contractAddress = "0x18AB37D940495dEC1EA35F2d76C08A4C7a390857";
 const contract = new Contract(contractAddress, contractABI, wallet);
 
-async function mintNFT() {
+async function mint() {
   let transaction = await contract.inscribe(
     "0x7b2270223a22656f732d3230222c226f70223a226d696e74222c227469636b223a22454f5349222c22616d74223a2231303030227d"
   );
@@ -29,13 +29,13 @@ async function runMultipleMints() {
 
   for (let i = 0; i < 100; i++) {
     console.log(`Minting ${i + 1}/100`);
-    await mintNFT().catch(console.error);
+    await mint().catch(console.error);
   }
 
   const endTime = new Date();
 
   const duration = (endTime - startTime) / 1000; // Convert milliseconds to seconds
-  console.log(`Finished minting 100 NFTs in ${duration} seconds.`);
+  console.log(`Finished minting 100 Smart Inscriptions in ${duration} seconds.`);
 }
 
 runMultipleMints();
